b9429e13e7b6e404645d322fe5a81c8f692cc6aa
Shreyysaxena <123328486+Shreyysaxena@users.noreply.github.com>
2026-02-06 18:44:22 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,25 @@
-import './App.css'; import { BrowserRouter, Routes, Route } from 'react-router-dom'; import { Toaster } from './components/ui/sonner'; import OrderTypePage from './pages/OrderTypePage'; import MenuPage from './pages/MenuPage'; import ReservationPage from './pages/ReservationPage'; function App() { return ( <div className="App"> <BrowserRouter> <Routes> <Route path="/" element={<OrderTypePage />} /> <Route path="/menu" element={<MenuPage />} /> <Route path="/reservation" element={<ReservationPage />} /> </Routes> </BrowserRouter> <Toaster position="top-center" /> </div> ); } export default App;
+import './App.css'; 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import { Toaster } from './components/ui/sonner'; 
+import OrderTypePage from './pages/OrderTypePage'; 
+import MenuPage from './pages/MenuPage'; 
+import ReservationPage from './pages/ReservationPage'; 
+
+function App() {
+  return (
+    <div className="min-h-screen overflow-x-hidden">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<OrderTypePage />} />
+          <Route path="/menu" element={<MenuPage />} />
+          <Route path="/reservation" element={<ReservationPage />} />
+        </Routes>
+      </BrowserRouter>
+
+      <Toaster position="top-center" />
+    </div>
+  );
+}
+
+
+export default App;
