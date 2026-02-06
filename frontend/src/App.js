e12e39a34708177380d45842c5bdf87cf545d6fa
Shreyysaxena <123328486+Shreyysaxena@users.noreply.github.com>
2026-02-06 18:28:16 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,1 @@
-function App() {
-  return (
-    <div className="min-h-screen overflow-x-hidden">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<OrderTypePage />} />
-          <Route path="/menu" element={<MenuPage />} />
-          <Route path="/reservation" element={<ReservationPage />} />
-        </Routes>
-      </BrowserRouter>
-
-      <Toaster position="top-center" />
-    </div>
-  );
-}
+function App() { return ( <div className="App"> <BrowserRouter> <Routes> <Route path="/" element={<OrderTypePage />} /> <Route path="/menu" element={<MenuPage />} /> <Route path="/reservation" element={<ReservationPage />} /> </Routes> </BrowserRouter> <Toaster position="top-center" /> </div> ); }
