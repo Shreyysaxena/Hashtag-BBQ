af637a29da51d94690c89aee7942c0f5818bf8be
Shreyysaxena <123328486+Shreyysaxena@users.noreply.github.com>
2026-02-06 18:26:36 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,6 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Toaster } from './components/ui/sonner';
-import OrderTypePage from './pages/OrderTypePage';
-import MenuPage from './pages/MenuPage';
-import ReservationPage from './pages/ReservationPage';
-
 function App() {
   return (
-    <div className="App">
+    <div className="min-h-screen overflow-x-hidden">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<OrderTypePage />} />
@@ -15,9 +8,8 @@ function App() {
           <Route path="/reservation" element={<ReservationPage />} />
         </Routes>
       </BrowserRouter>
+
       <Toaster position="top-center" />
     </div>
   );
 }
-
-export default App;
